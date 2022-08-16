bf8af4270f5bb8869e056a932871472f3df22bdc
buudee625 <liu.andrew625@gmail.com>
2022-08-15 23:03:46 -0700

    Only instruments that are added in the Instrument model will popular in new musician creation page

--- controllers/musicians.js
+++ controllers/musicians.js
@@ -1,4 +1,5 @@
-const Musicians = require("../models/musicians");
+const Musicians = require('../models/musicians');
+const Instrument = require('../models/instruments');
 
 module.exports = {
   index,
@@ -9,35 +10,46 @@ module.exports = {
 
 async function index(req, res) {
   const musicianDoc = await Musicians.find();
+  const instDoc = await Instrument.find();
+  // console.log(instDoc, '<- instDoc: ctrl/musicians/index()');
   try {
-    res.render("../views/musicians/index.ejs", {
+    res.render('../views/musicians/index.ejs', {
       musicians: musicianDoc,
-      title: "Jaw Dropping Musicians",
+      instruments: instDoc,
+      title: 'Jaw Dropping Musicians',
     });
   } catch (err) {
-    res.render(err);
+    return res.render(err);
   }
 }
 
-function newMusician(req, res) {
-  res.render("../views/musicians/new.ejs");
+async function newMusician(req, res) {
+  try {
+    const instDoc = await Instrument.find();
+    console.log(instDoc, '<- instDoc: ctrl/musicians/newMusician()');
+    res.render('../views/musicians/new.ejs', {
+      instruments: instDoc,
+    });
+  } catch (err) {
+    return res.render(err);
+  }
 }
 
 function create(req, res) {
   Musicians.create(req.body, function (err, musicianDoc) {
     if (err) {
-      console.log(err, "<- err from controller/musicians/create()");
-      return res.render("../views/musicians/new.ejs");
+      console.log(err, '<- err from controller/musicians/create()');
+      return res.render('../views/musicians/new.ejs');
     }
-    console.log(musicianDoc, "<- musicianDoc from create()");
-    res.redirect("/jdmusicians");
+    console.log(musicianDoc, '<- musicianDoc from create()');
+    res.redirect('/jdmusicians');
   });
 }
 
 async function show(req, res) {
   const musicianDoc = await Musicians.findById(req.params.id);
-  console.log(req.params.id, "<- req._id from show()");
-  res.render("../views/musicians/show.ejs", {
+  // console.log(req.params.id, '<- req.params.id: ctrl/musicians/show()');
+  res.render('../views/musicians/show.ejs', {
     musicians: musicianDoc,
   });
 }
