957f7f2d4a50a3569a2c3bfc37f0c72e1606528e
buudee625 <liu.andrew625@gmail.com>
2022-08-18 21:44:24 -0700

    video carousel function implemented

--- controllers/videos.js
+++ controllers/videos.js
@@ -4,12 +4,21 @@ module.exports = {
   create,
 };
 
+function extractVideoId(url) {
+  const regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  const match = url.match(regExp);
+  if (match && match[7].length == 11) {
+    return match[7];
+  }
+}
+
 async function create(req, res) {
   try {
     const musicianDoc = await Musician.findById(req.params.id);
     // console.log(req.body, '<- req.body: ctrl/videos/create()');
     // console.log(req.body.url, '<- req.body.url: ctrl/videos/create()');
-    // console.log(musicianDoc, '<- musicianDoc: ctrl/videos/create()');
+    req.body.url = extractVideoId(req.body.url);
     musicianDoc.video.push(req.body);
     await musicianDoc.save();
     res.redirect(`/jdmusicians/${req.params.id}`);
